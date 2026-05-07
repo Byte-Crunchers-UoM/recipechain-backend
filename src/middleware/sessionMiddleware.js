12103a73f59ff62b56f9d4bea4733cd29e95e82c
Pasindu Sri Induwara Madhushan <ipasindusri@gmail.com>
2026-05-07 14:48:58 +0530

    Clean up comments in optionalSession function
    Removed commented code in optionalSession function.

--- src/middleware/sessionMiddleware.js
+++ src/middleware/sessionMiddleware.js
@@ -24,10 +24,10 @@ export const requireSession = (req, res, next) => {
   }
 };
 export const optionalSession = (req, res, next) => {
-    // 1. හරියටම requireSession එකේ වගේම rc_session කුකිය ගන්නවා
+    
     const token = req.cookies.rc_session;
     
-    // (අවශ්‍ය නම් Header එකෙනුත් ගන්න පුළුවන් විදියට තියාගන්න)
+    
     // const token = req.cookies.rc_session || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
 
     if (!token) {
@@ -35,7 +35,6 @@ export const optionalSession = (req, res, next) => {
     }
 
     try {
-        // 2. මෙතන 'process.env.SESSION_SECRET' වෙන්න ඕනේ, 'JWT_SECRET' නෙවෙයි (requireSession එකේ තියෙන්නෙත් එහෙමයි)
         const decoded = jwt.verify(token, process.env.SESSION_SECRET);
         
         req.user = decoded; 
